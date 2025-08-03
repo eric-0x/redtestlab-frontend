1f636c648bde7f9778397cb30330ed2614ab718e
Eric SA <eric.2002@srit.org>
2025-08-03 22:02:01 +0530

    Refactor code structure for improved readability and maintainability

---
+++ components/HomeClient.tsx
@@ -0,0 +1,52 @@
+// components/HomeClient.tsx
+"use client";
+
+import { useState, useEffect } from "react";
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import CategoryShowcase from "@/components/CategoryShowcase";
+import HealthPackageCurosel from "@/components/HealthPackageCurosel";
+import ServiceFeatures from "@/components/ServiceFeatures";
+import WomenHealthPackages from "@/components/WomenHealthPackages";
+import CustomPackage from "@/components/CustomPackage";
+import ScanList from "@/components/ScanList";
+import HospitalBanner from "@/components/HospitalBanner";
+import PlayStoreCard from "@/components/PlayStoreCard";
+import Slider from "@/components/Slider";
+import Testimonial from "@/components/Testimonial";
+import Footer from "@/components/Footer";
+import ProfileSidebar from "@/components/ProfileSidebar";
+
+export default function HomeClient() {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      if (localStorage.getItem("openProfileSidebar") === "true") {
+        setSidebarOpen(true);
+        localStorage.removeItem("openProfileSidebar");
+      }
+    }
+  }, []);
+
+  return (
+    <div>
+      <Header />
+      <Hero />
+      <CategoryShowcase />
+      <HealthPackageCurosel />
+      <ServiceFeatures />
+      <WomenHealthPackages />
+      <CustomPackage />
+      <ScanList />
+      <HospitalBanner />
+     
+      <Slider />
+       <PlayStoreCard />
+      <Testimonial />
+      
+      <Footer />
+      <ProfileSidebar isOpen={sidebarOpen} onClose={() => setSidebarOpen(false)} />
+    </div>
+  );
+}
