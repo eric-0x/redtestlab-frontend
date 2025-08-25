835e8c520e388d37e5e8321f55a930ddf6486120
root <root@srv892052.hstgr.cloud>
2025-08-25 13:59:42 +0000

    WIP: local changes before pull

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
+  trailingSlash: true,
   images: {
     domains: ["res.cloudinary.com"],
-  },
+    unoptimized: true
+  }
 };
 
 export default nextConfig;
+
