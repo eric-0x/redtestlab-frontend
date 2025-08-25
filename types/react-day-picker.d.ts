c275f881a45525c583181c5547d9f8f3b7aaec39
Eric S A <eric2025sa@gmail.com>
2025-08-25 20:12:16 +0530

    feat: add type definitions for react-day-picker and update Calendar component props

---
+++ types/react-day-picker.d.ts
@@ -0,0 +1,6 @@
+declare module 'react-day-picker' {
+  import * as React from 'react'
+  export const DayPicker: React.ComponentType<any>
+  export type DayPickerProps = any
+  export default DayPicker
+}
